e007bead79b76b4dd3d704b64094457cb1eac3c5
misindelka <misindelka.dev@gmail.com>
2021-05-20 13:47:23 +0200

    Revert "GetTaskGroups"
    This reverts commit ce850b26ce204ab86531dfb33a1bc29a9b275a17.

--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -2,7 +2,11 @@ import * as React from 'react'
 import { useParams } from 'react-router-dom'
 import { Box, Grid, Button, Text } from '@chakra-ui/react'
 import { getBoard } from '../utils/api'
+<<<<<<< HEAD
 import TaskGrpups from './components/TaskGroups'
+=======
+import { TaskGroupColumn } from './components/TaskGrupColumn'
+>>>>>>> parent of ce850b2... GetTaskGroups
 
 const Board = () => {
   const { id } = useParams()
@@ -28,10 +32,15 @@ const Board = () => {
       <Text p="3" fontWeight="bold">
         {board.name}
       </Text>
+<<<<<<< HEAD
       <Grid mt="3" templateColumns={{ base: '1fr 1fr', lg: '1fr 1fr 1fr 1fr' }}>
         <TaskGrpups boardId={board.id} />
         <TaskGrpups boardId={board.id} />
 
+=======
+      <Grid gridTemplateColumns={['auto', '15rem auto']} gap="4">
+        <TaskGroupColumn />
+>>>>>>> parent of ce850b2... GetTaskGroups
         <Box m="4">
           <Button colorScheme="blue">+ Add new group </Button>
         </Box>
