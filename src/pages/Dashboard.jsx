a2177e9afb7977b7c5a25caae67afa34e307fdf9
Vladimír Tomko <tomko@tomkowebstranky.sk>
2021-05-20 08:25:00 +0200

    Changed Dashboard.jsxk and BrandLogo.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,32 @@
 import * as React from 'react'
-import { Box, Center } from '@chakra-ui/react'
+import { Box, Button, Center, Heading } from '@chakra-ui/react'
+import TextAnimation from 'react-animate-text'
+import { Link } from 'react-router-dom'
+import reactBg from '../assets/react_bg.png'
 
 const Dashboard = () => {
   return (
-    <Box h="100vh">
-      <Center h="100%" />
+    <Box background={`url('${reactBg}') center / cover no-repeat`} h="92vh">
+      <Center h="30vh">
+        <div>
+          <Heading fontSize={['3xl', '5xl']} color="white" textAlign="center" mb="4">
+            <TextAnimation>Welcome to ReactBoard!</TextAnimation>
+          </Heading>
+        </div>
+      </Center>
+      <Center>
+        <Button
+          color="white"
+          mt="45vh"
+          w="300px"
+          size="lg"
+          variant="outline"
+          as={Link}
+          to="/boards"
+        >
+          Let's see some boards
+        </Button>
+      </Center>
     </Box>
   )
 }
