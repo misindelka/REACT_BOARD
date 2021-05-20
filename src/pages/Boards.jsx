328a183f6f5f1be735b8a98bd879434e3f3cba55
Vladimír Tomko <tomko@tomkowebstranky.sk>
2021-05-20 13:04:11 +0200

    Add boards, change name and color

--- src/pages/Boards.jsx
+++ src/pages/Boards.jsx
@@ -1,64 +1,161 @@
 import * as React from 'react'
 import { Link } from 'react-router-dom'
-import { Box, Grid } from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  Center,
+  FormControl,
+  Grid,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Select,
+  Spinner,
+  useToast,
+  useDisclosure,
+} from '@chakra-ui/react'
+import { PlusSquareIcon } from '@chakra-ui/icons'
 // import { id } from 'date-fns/locale'
-
-import { getBoards } from '../utils/api'
+import { getBoards, createBoard } from '../utils/api'
 
 const Boards = () => {
   // eslint-disable-next-line no-unused-vars
   const [status, setStatus] = React.useState('loading')
   const [boards, setBoards] = React.useState([])
+  const [boardName, setBoardName] = React.useState('')
+  const [boardColor, setBoardColor] = React.useState('blue')
+
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  const toast = useToast()
+
+  const initialRef = React.useRef()
+  const finalRef = React.useRef()
 
   React.useEffect(() => {
     try {
       const fetchData = async () => {
         const data = await getBoards()
+        setStatus('done')
         setBoards(data)
       }
       fetchData()
     } catch (e) {
       //  do nothing
     }
-  }, [])
+  }, [boardName, boardColor])
 
-  return (
-    <Grid gridTemplateColumns={['auto', '15rem auto']} gap="4">
+  function handleAddBoardName(event) {
+    event.preventDefault()
+    setBoardName(event.target.value)
+  }
+
+  function handleAddNewBoard() {
+    if (boardName === '') {
+      toast({
+        title: 'You need to input the board name.',
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      })
+    } else {
+      createBoard(boardName, boardColor)
+      setBoardName('')
+      onClose()
+    }
+  }
+  return status !== 'done' ? (
+    <Center>
+      <Spinner
+        m="33vh"
+        thickness="4px"
+        speed="1s"
+        emptyColor="blue.200"
+        color="blue.500"
+        size="xl"
+      />
+    </Center>
+  ) : (
+    <Grid mt="3" gridTemplateColumns={{ base: '1fr 1fr', lg: '1fr 1fr 1fr 1fr' }}>
       {boards.map((board) => (
         <Box
+          display="flex"
+          p="4"
+          justifyContent="center"
+          alignItems="center"
+          minH="10vh"
           as={Link}
+          m="3"
           to={`/boards/${board.id}`}
           key={board.id}
-          m="2"
-          maxW="xs"
-          minH="40"
-          borderWidth="1px"
           borderRadius="lg"
           overflow="hidden"
-          backgroundColor="gray.200"
+          backgroundColor={board.color}
           shadow="2"
+          color="white"
         >
-          <Box p="3">
-            <Box d="flex" alignItems="center">
-              <Box
-                fontWeight="bold"
-                letterSpacing="wide"
-                fontSize="md"
-                textTransform="uppercase"
-                ml="2"
-              >
-                {board.name}
-              </Box>
-            </Box>
-            <Box m="3" letterSpacing="wide" fontSize="xl" ml="2">
-              number: {board.id}
-            </Box>
-            <Box m="3" bottom="1.5" fontSize="xl" ml="2">
-              by: misindelka@dev
-            </Box>
+          <Box
+            textAlign="center"
+            fontWeight="bold"
+            letterSpacing="wide"
+            fontSize="md"
+            textTransform="uppercase"
+          >
+            {board.name}
           </Box>
         </Box>
       ))}
+      <Button m="3" leftIcon={<PlusSquareIcon />} variant="outline" onClick={onOpen}>
+        Add new board
+      </Button>
+
+      <Modal
+        initialFocusRef={initialRef}
+        finalFocusRef={finalRef}
+        isOpen={isOpen}
+        onClose={onClose}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Create new board</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl mt={4}>
+              <Input
+                required
+                variant="flushed"
+                value={boardName}
+                onChange={(e) => handleAddBoardName(e)}
+                placeholder="Board Name"
+              />
+            </FormControl>
+            <FormControl mt={4}>
+              <Select
+                defaultValue="blue.400"
+                onChange={(e) => setBoardColor(e.target.value)}
+                variant="flushed"
+                placeholder="Board color"
+              >
+                <option value="blue.400">Blue</option>
+                <option value="red.400">Red</option>
+                <option value="green.400">Green</option>
+              </Select>
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button onClick={handleAddNewBoard} colorScheme="blue" mr={3}>
+              Create board
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Grid>
   )
 }
